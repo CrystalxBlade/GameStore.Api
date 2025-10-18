588aba29132abc8672b7fe9ca35d69050bdedcf4
CrystalxBlade <satish141m@gmail.com>
2025-10-18 23:41:01 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using GameStore.API.Dtos;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<GameDto> games = [
+    new(1,
+    "Tekken 8",
+    "Fighting",
+    20.49M,
+    new DateOnly(2024, 7, 15)),
+    new(2, 
+    "Fifa 25", 
+    "Sports", 
+    45.60M, 
+    new DateOnly(2025, 1, 14)),
+    new(3, 
+    "GTA V", 
+    "RPG", 
+    100M, 
+    new DateOnly(2013, 5, 2))
+];
+
+app.MapGet("games", () => games);
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
